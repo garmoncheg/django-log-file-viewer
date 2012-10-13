04a3a8510c9fdceea4fd2485aa2cd44b54dfa2cd
Iurii Garmash <garmon1@gmail.com>
2012-10-13 20:23:39 +0300

    Version 0.4 released

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ Logging for:
 """
 import os
 import re
+import linecache
 
 from django.db import models
 
@@ -27,25 +28,43 @@ class LogFilesManager(object):
         return file_list
 
     def get_file(self, file_full_path):
-        fobj = open(file_full_path)
+        fobj = open(file_full_path, 'rb')
         return fobj
 
-    def parse_log_file(self, logfile, from_line=0):
+    def get_file_lines_count(self, file_obj):
+        """Creates fake log file list (without real lines, but with proper length)"""
+        fake_log_file = []
+        log_file_fake_lines = file_obj.xreadlines()
+        count = 0
+        for line in log_file_fake_lines:
+            fake_log_file.append(count)
+            count += 1
+        if fake_log_file:
+            fake_log_file.append(count)
+        return fake_log_file
+
+    def compile_re_index(self, regexp=None):
+        """Creating Regexp prog to match entries"""
+        if not regexp:
+            regexp = LOG_FILES_RE
+        prog = re.compile(regexp)
+        return prog
+
+    def parse_log_file(self, logfile, from_line=0, to_line=LOG_FILES_PAGINATE_LINES, full=False):
         """Returns parsed read file
 
         in form of entry names header (taken from Rgex group names)
         and lines tuples list"""
-        # Reading file
-        logfile.seek(0)
-        read_file = logfile.readlines()
-        # Creating Regexp prog to match entries
         file_dict = []
-        prog = re.compile(LOG_FILES_RE)
+        prog = self.compile_re_index()
         # Reading amount of lines
         line_num = from_line
-        for count in range(LOG_FILES_PAGINATE_LINES):
+        if full:
+            file_obj = self.get_file(logfile)
+            to_line = self.get_file_lines_count(file_obj).__len__()
+        for count in range(to_line):
             try:
-                line = read_file[line_num]
+                line = linecache.getline(logfile, line_num)
                 matches_set = prog.findall(str(line))
                 file_dict.append(matches_set)
                 line_num += 1
@@ -53,10 +72,11 @@ class LogFilesManager(object):
                 # log file is shorter then LOG_FILES_PAGINATE_LINES or
                 # amount of lines smaller then from_line left
                 pass
+        return file_dict
 
-        # Making file length data
-        file_len = read_file.__len__
-        # Making logfile indexes header
+    def compile_header_from_regexp(self, regexp=None):
+        """Making logfile indexes header"""
+        prog = self.compile_re_index()
         header_length = prog.groups
         header_list = []
         if prog.groupindex:
@@ -64,7 +84,7 @@ class LogFilesManager(object):
                 header_list.append(number)
             for group_name, index in prog.groupindex.iteritems():
                 header_list[int(index) - 1] = group_name
-        return (file_len, header_list, file_dict)
+        return header_list
 
 class string_with_title(str):
     def __new__(cls, value, title):
--- views.py
+++ views.py
@@ -1,5 +1,6 @@
 import os, csv
 from django.http import HttpResponse
+from django.core.paginator import Paginator
 from django.shortcuts import render_to_response
 from django.http import HttpResponseBadRequest
 from django.contrib.auth.decorators import login_required, user_passes_test
@@ -26,19 +27,37 @@ def logfiles_list(request, template_name='listfiles.html'):
 @user_passes_test(lambda u: u.is_superuser)
 def logfile_view(request, logfile_id, template_name='logfile.html'):
     """Returns a list of Log File content parsed by main regexp"""
+    page = request.GET.get('page')
+    if not page:
+        page = 1
+    else:
+        try:
+            page = int(page)
+        except ValueError:
+            page = 1
+            pass
+
     manager = LogFilesManager()
     files_list = manager.list_logfiles(LOG_FILES_DIR)
     try:
         filename = files_list[int(logfile_id)]
     except Exception:
         return HttpResponseBadRequest()
-    logfile = manager.get_file(os.path.join(LOG_FILES_DIR, files_list[int(logfile_id)]))
-    logfile_lines, header_list, file_dict = manager.parse_log_file(logfile)
+    logfile = os.path.join(LOG_FILES_DIR, files_list[int(logfile_id)])
+    log_file_object = manager.get_file(logfile)
+    fake_log_file_lines = manager.get_file_lines_count(log_file_object)
+    paginator = Paginator(fake_log_file_lines, LOG_FILES_PAGINATE_LINES)
+    paginated_lines = paginator.page(page)
+    first_line = paginated_lines.object_list[0]
+    current_file_dict = manager.parse_log_file(logfile, first_line)
+    paginated_lines.object_list = current_file_dict
+    header_list = manager.compile_header_from_regexp()
     context = {
-            'file_dict': file_dict,
             'header_list': header_list,
             'file_name': filename,
             'file_id': logfile_id,
+            'paginator': paginated_lines,
+            'page': page,
     }
     return render_to_response(template_name, context)
 
@@ -52,9 +71,10 @@ def logfile_to_csv(request, logfile_id):
         filename = files_list[int(logfile_id)]
     except Exception:
         return HttpResponseBadRequest()
-    logfile = manager.get_file(os.path.join(LOG_FILES_DIR, files_list[int(logfile_id)]))
-    logfile_lines, header_list, file_dict = manager.parse_log_file(logfile)
+    logfile = os.path.join(LOG_FILES_DIR, files_list[int(logfile_id)])
+    file_dict = manager.parse_log_file(logfile, 0, full=True)
     # Constructing CSV file
+    header_list = manager.compile_header_from_regexp()
     response = HttpResponse(mimetype='text/csv')
     response['Content-Disposition'] = 'attachment; filename=%s.csv' % filename
     writer = csv.writer(response)
